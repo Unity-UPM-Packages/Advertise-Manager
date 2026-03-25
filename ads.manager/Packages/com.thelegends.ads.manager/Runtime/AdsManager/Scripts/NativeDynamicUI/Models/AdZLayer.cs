dfedea75f14fe3d4c3034a7dd2749cd5332fb6fa
QuangHaiVu <vuquanghai0108@gmail.com>
2026-03-25 09:15:02 +0700

    giai đoạn 1

---
+++ ads.manager/Packages/com.thelegends.ads.manager/Runtime/AdsManager/Scripts/NativeDynamicUI/Models/AdZLayer.cs
@@ -0,0 +1,10 @@
+namespace TheLegends.Base.Ads.NativeDynamicUI
+{
+    public enum AdZLayer
+    {
+        BannerLayer = 10,
+        MRECLayer = 20,
+        PopupLayer = 50,
+        FullscreenLayer = 100
+    }
+}
---
+++ ads.manager/Packages/com.thelegends.ads.manager/Runtime/AdsManager/Scripts/NativeDynamicUI/Models/NativeAdLayoutConfig.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheLegends.Base.Ads.NativeDynamicUI
+{
+    [Serializable]
+    public class NativeAdLayoutConfig
+    {
+        public string layoutId;
+        public List<NativeAdElementConfig> elements = new List<NativeAdElementConfig>();
+    }
+
+    [Serializable]
+    public class NativeAdElementConfig
+    {
+        public string elementType; // String representation of NativeAdElement
+        
+        public RectTransformConfig rectTransform;
+        public ImageConfig image;
+        public TextConfig text;
+    }
+
+    [Serializable]
+    public class RectTransformConfig
+    {
+        public Vector2 anchorMin;
+        public Vector2 anchorMax;
+        public Vector2 offsetMin;
+        public Vector2 offsetMax;
+        public Vector2 pivot;
+        public float rotationZ;
+        public float scaleX;
+        public float scaleY;
+    }
+
+    [Serializable]
+    public class ImageConfig
+    {
+        public string color; // Hex format #RRGGBBAA
+        public float cornerRadius;
+        public string imagePath; // Absolute path to physical file
+        public bool isRadialFill;
+    }
+
+    [Serializable]
+    public class TextConfig
+    {
+        public string textContent;
+        public string color;
+        public float fontSize;
+        public string alignment; // e.g., "MiddleCenter"
+        public bool isBold;
+        public bool isItalic;
+        public float lineSpacing;
+    }
+}
